76a628a7fc920c103c51aa5435fdf3e52f514b1a
locky182 <oven8209@gmail.com>
2026-02-16 13:34:21 +0300

    1_commit_clear_project
    Чистый проект

---
+++ src/main/java/com/locky182/reservation/ReservationApplication.java
@@ -0,0 +1,13 @@
+package com.locky182.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
